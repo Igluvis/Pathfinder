4754a019ea5b4263b08c67ccc867ce73d95f43c4
Igluvis <hangard.jamts@gmail.com>
2022-06-28 21:08:02 +0200

    added space as a* start

--- src/core/main.py
+++ src/core/main.py
@@ -2,6 +2,7 @@ import pygame
 from core.utils.grid import Grid
 from core.pygame.visualize import draw
 from core.pygame.misc import get_node
+from core.pathfinder.a_star import a_star
 
 def main(win, width, height, rows, cols):
     '''
@@ -28,29 +29,43 @@ def main(win, width, height, rows, cols):
             if event.type == pygame.QUIT:
                 run = False
 
+            # Q: set Start node
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_q:
                     if start:
                         start.cleanup()
                     node = get_node(pygame.mouse.get_pos(), grid, width=width//cols, height=height//rows)
                     node.set_start()
-                    start = node
+                    start = node           
 
+            # E: set End node
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_e:
                     if end:
                         end.cleanup()
                     node = get_node(pygame.mouse.get_pos(), grid, width=width//cols, height=height//rows)
                     node.set_end()
+                    end = node
 
-            # Draw wall with left mouse click
+            # left mouse: set wall node
             if pygame.mouse.get_pressed()[0]:
                 node = get_node(pygame.mouse.get_pos(), grid, width=width//cols, height=height//rows)
                 node.set_wall()
 
-            # empty node with right click
+            # right mouse: set walk node
             if pygame.mouse.get_pressed()[2]:
                 node = get_node(pygame.mouse.get_pos(), grid, width=width//cols, height=height//rows)
                 node.set_walkable()
+
+            # Space: A* algo
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE and start and end:
+                    a_star(
+                        lambda: draw(win, grid, width, height, rows, cols),
+                        start=start,
+                        end=end,
+                        grid=grid
+                    )
+
 
     pygame.quit()
